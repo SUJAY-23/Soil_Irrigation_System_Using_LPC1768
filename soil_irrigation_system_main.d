ce97bded9fa28b58a190002c189c28c06eb6d88d
Sujay Behera <77610345+SUJAY-23@users.noreply.github.com>
2024-03-06 23:17:13 +0530

    Add files via upload

---
+++ soil_irrigation_system_main.d
@@ -0,0 +1,9 @@
+soil_irrigation_system_main.o: Soil_Irrigation_System_Main.c
+soil_irrigation_system_main.o: D:\KPIT\PFile\ARM\INC\NXP\LPC17xx\LPC17xx.h
+soil_irrigation_system_main.o: D:\KPIT\PFile\ARM\RV31\INC\core_cm3.h
+soil_irrigation_system_main.o: D:\KPIT\PFile\ARM\RV31\INC\stdint.h
+soil_irrigation_system_main.o: D:\KPIT\PFile\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
+soil_irrigation_system_main.o: D:\KPIT\PFile\ARM\RV31\INC\stdio.h
+soil_irrigation_system_main.o: uart.h
+soil_irrigation_system_main.o: lcd.c
+soil_irrigation_system_main.o: lcd.h
